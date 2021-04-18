691d90ae54661d9ca61386bb2119543a51a6d289
Denis <pdenisok@mail.ru>
2021-04-18 14:09:26 +0300

    0.3.1

--- src/Helper.php
+++ src/Helper.php
@@ -12,25 +12,9 @@ use denisok94\helper\traits\StringHelper;
  * Helper
  * @author Denisok94
  * @link https://s-denis.ru/git/helper
- * @version 0.2
+ * @version 0.3
  */
 class Helper
 {
     use ArrayHelper, DataHelper, HtmlHelper, OtherHelper, StringHelper;
-}
-
-//--------------------------------------------------------
-
-use denisok94\helper\traits\yii2\MetaTag;
-use denisok94\helper\traits\yii2\Other;
-
-/**
- * YiiHelper
- * @author Denisok94
- * @link https://s-denis.ru/git/helper
- * @version 0.2
- */
-class YiiHelper extends Helper
-{
-    use MetaTag, Other;
 }
---
+++ src/YiiHelper.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace denisok94\helper;
+
+use denisok94\helper\traits\yii2\MetaTag;
+use denisok94\helper\traits\yii2\Other;
+use denisok94\helper\Helper;
+
+/**
+ * YiiHelper
+ * @author Denisok94
+ * @link https://s-denis.ru/git/helper
+ * @version 0.3.1
+ */
+class YiiHelper extends Helper
+{
+    use MetaTag, Other;
+}
