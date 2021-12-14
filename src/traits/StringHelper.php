cc14ddf3de47b3491f692eabe2fc1d08bb01efde
Denis <pdenisok@mail.ru>
2021-12-14 14:37:42 +0300

    StringHelper. add slug, ru2Lat, ruToLat, ru2Slug

--- src/traits/StringHelper.php
+++ src/traits/StringHelper.php
@@ -72,4 +72,66 @@ trait StringHelper
         $cases = [2, 0, 1, 1, 1, 2];
         return $titles[($num % 100 > 4 && $num % 100 < 20) ? 2 : $cases[min($num % 10, 5)]];
     }
+
+    /**
+     * человекопонятный URL
+     * @param string $source
+     * @return string
+     */
+    public static function slug($source)
+    {
+        // очистить от лишних пробелов, преобразовать в нижний регистр.
+        $title = mb_strtolower(trim($source), 'UTF-8');
+        // заменить пробелы, знаки пунктуации и прочие "не буквы": дефисом
+        return preg_replace('/[^a-z0-9]+/i', '-', $title);
+    }
+
+    /**
+     * Транслитирование, ГОСТ 7.79-2000, схема А
+     * @param string $source
+     * @return string
+     */
+    public static function ru2Lat($source)
+    {
+        if ($source) {
+            $rus = [
+                'А', 'Б', 'В', 'Г', 'Д', 'Е', 'Ё', 'Ж', 'З', 'И', 'Й', 'К', 'Л', 'М', 'Н', 'О', 'П', 'Р', 'С', 'Т', 'У', 'Ф', 'Х', 'Ц', 'Ч', 'Ш', 'Щ', 'Ъ', 'Ы', 'Ь', 'Э', 'Ю', 'Я',
+                'а', 'б', 'в', 'г', 'д', 'е', 'ё', 'ж', 'з', 'и', 'й', 'к', 'л', 'м', 'н', 'о', 'п', 'р', 'с', 'т', 'у', 'ф', 'х', 'ц', 'ч', 'ш', 'щ', 'ъ', 'ы', 'ь', 'э', 'ю', 'я'
+            ];
+            $lat = [
+                'A', 'B', 'V', 'G', 'D', 'E', 'Yo', 'Zh', 'Z', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'R', 'S', 'T', 'U', 'F', 'H', 'C', 'Ch', 'Sh', 'Shch', 'Y', 'Y', 'Y', 'E', 'Yu', 'Ya',
+                'a', 'b', 'v', 'g', 'd', 'e', 'yo', 'zh', 'z', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'r', 's', 't', 'u', 'f', 'h', 'c', 'ch', 'sh', 'shch', 'y', 'y', 'y', 'e', 'yu', 'ya'
+            ];
+            return str_replace($rus, $lat, $source);
+        }
+    }
+
+    /**
+     * Транслитирование, ГОСТ 7.79-2000, схема Б
+     * @param string $source
+     * @return string
+     */
+    public static function ruToLat($source)
+    {
+        if ($source) {
+            $rus = [
+                'А', 'Б', 'В', 'Г', 'Д', 'Е', 'Ё', 'Ж', 'З', 'И', 'Й', 'К', 'Л', 'М', 'Н', 'О', 'П', 'Р', 'С', 'Т', 'У', 'Ф', 'Х', 'Ц', 'Ч', 'Ш', 'Щ', 'Ъ', 'Ы', 'Ь', 'Э', 'Ю', 'Я',
+                'а', 'б', 'в', 'г', 'д', 'е', 'ё', 'ж', 'з', 'и', 'й', 'к', 'л', 'м', 'н', 'о', 'п', 'р', 'с', 'т', 'у', 'ф', 'х', 'ц', 'ч', 'ш', 'щ', 'ъ', 'ы', 'ь', 'э', 'ю', 'я'
+            ];
+            $lat = [
+                'A', 'B', 'V', 'G', 'D', 'E', 'Yo', 'Zh', 'Z', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'R', 'S', 'T', 'U', 'F', 'H', 'C', 'Ch', 'Sh', 'Shh', '``', 'Y`', '`', 'E`', 'Yu', 'Ya',
+                'a', 'b', 'v', 'g', 'd', 'e', 'yo', 'zh', 'z', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'r', 's', 't', 'u', 'f', 'h', 'c', 'ch', 'sh', 'shh', '``', 'y`', '`', 'e`', 'yu', 'ya'
+            ];
+            return str_replace($rus, $lat, $source);
+        }
+    }
+
+    /**
+     * @param string $source
+     * @return string
+     */
+    public static function ru2Slug($source)
+    {
+        return StringHelper::slug(StringHelper::ru2Lat($source));
+    }
 }
