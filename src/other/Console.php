b3b8bb6832d7680d5fce9c1123200375073b88e0
Denis Popov <giorgi-gi.pov@mail.ru>
2026-07-19 01:55:43 +0300

    [+] show_status() in  class Console

--- src/other/Console.php
+++ src/other/Console.php
@@ -313,7 +313,7 @@ class Console
     /**
      * Статус в консоли
      * @param string $echo
-     * @return string
+     * @return void
      * ```php
      * for ($x = 1; $x <= 100; $x++) {
      *  Console::show($x);
@@ -325,5 +325,67 @@ class Console
     {
         echo "\r$echo ";
         flush();
+    }
+
+    /**
+     * show a status bar in the console
+     * 
+     * ```php
+     * for ($x = 1; $x <= 100; $x++) {
+     *     Console::show_status($x, 100);
+     *     usleep(100000);
+     * }
+     * ```
+     *
+     * @param int $done   how many items are completed
+     * @param int $total  how many items are to be done total
+     * @param int $size   optional size of the status bar
+     * @return void
+     * @author Brian Moon
+     * @link https://snipplr.com/view/29548/command-line-progress-bar-with-php
+     */
+    function show_status($done, $total, $size = 30)
+    {
+        static $start_time;
+
+        // if we go over our bound, just ignore it
+        if ($done > $total) return;
+
+        if (empty($start_time)) $start_time = time();
+        $now = time();
+
+        $perc = (float)($done / $total);
+
+        $bar = floor($perc * $size);
+
+        $status_bar = "\r[";
+        $status_bar .= str_repeat("=", $bar);
+        if ($bar < $size) {
+            $status_bar .= ">";
+            $status_bar .= str_repeat(" ", $size - $bar);
+        } else {
+            $status_bar .= "=";
+        }
+
+        $disp = number_format($perc * 100, 0);
+
+        $status_bar .= "] $disp%  $done/$total";
+
+        $rate = ($now - $start_time) / $done;
+        $left = $total - $done;
+        $eta = round($rate * $left, 2);
+
+        $elapsed = $now - $start_time;
+
+        $status_bar .= " remaining: " . number_format($eta) . " sec.  elapsed: " . number_format($elapsed) . " sec.";
+
+        echo "$status_bar  ";
+
+        flush();
+
+        // when done, send a newline
+        if ($done == $total) {
+            echo "\n";
+        }
     }
 }
